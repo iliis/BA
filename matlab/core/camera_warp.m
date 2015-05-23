a5990fc602d9c8d3df925bd8ddc3d7f0013abcaf
iliis <samuelbryner@gmx.ch>
2015-05-23 23:23:00 +0200

    Implement camera_warp(): combines all the core functions.
    Unfortunately, it's not very clean (flipped V-axis, no tests)

---
+++ matlab/core/camera_warp.m
@@ -0,0 +1,102 @@
+function [ errors, J_warp ] = camera_warp( image_keyframe, depths_keyframe, image_current, T, intrinsics )
+% projects points into world, transforms and projects back
+%
+% TODO: write some tests for this function! There are some critical things
+% with the inverted V-axis...
+%
+% INPUT:
+%
+% image_keyframe:
+%   H * W intensity image, this will be warped
+%
+% depths_keyframe:
+%   H * W depths of keyframe image
+%
+% image_current:
+%   H * W intensity image, this is the target
+%
+% T:
+%   [x y z alpha beta gamma]' transformation to apply
+%
+% intrinsics:
+%   camera intrinsics
+% 
+
+[H, W] = size(image_keyframe);
+
+calc_jacobian = nargout > 1;
+
+% check input parameters
+assert(all(size(depths_keyframe) == [H,W]), 'depth matrix must have same size as intensity image');
+assert(all(size(image_current)   == [H,W]), 'current image must have same size as keyframe image');
+assert(numel(T) == 6);
+assert(isa(intrinsics, 'CameraIntrinsics'));
+
+
+[X,Y] = meshgrid(1:W, 1:H);
+
+points_keyframe_camera = image_to_list(cat(3,X,Y));
+
+% attention: inverted V-Axis -> flip depths!
+points_world = camera_project_inverse(points_keyframe_camera, image_to_list(flipud(depths_keyframe)), intrinsics);
+
+if calc_jacobian
+    [points_current, J_T] = camera_transform(points_world, T);
+    [points_current_camera, J_P] = camera_project(points_current, intrinsics);
+else
+    points_current        = camera_transform(points_world, T);
+    points_current_camera = camera_project(points_current, intrinsics);
+end
+
+
+
+% filter points outside of image range
+valid_points              = is_in_img_range(points_current_camera, image_current);
+points_current_camera     = points_current_camera(:, valid_points);
+
+if calc_jacobian
+    [intensities_current, J_I] = camera_intensity_sample(points_current_camera, image_current);
+else
+    intensities_current = camera_intensity_sample(points_current_camera, image_current);
+end
+
+% only compare points that projected inside the current image
+intensities_keyframe = camera_intensity_sample(points_keyframe_camera, image_keyframe); %image_to_list(flipud(image_keyframe));
+intensities_keyframe = intensities_keyframe(:, valid_points);
+errors = (intensities_keyframe - intensities_current).^2;
+
+if calc_jacobian
+    N = size(points,1);
+    assert(N == size(J_T,3));
+    assert(N == size(J_P,3));
+    assert(N == size(J_I,3));
+    J_warp = zeros(N,numel(T));
+    % this seems to be one of the fastest way of doing N matrix multiplications:
+    for i = 1:N
+        J_warp(i,:) = J_I(:,:,i) * J_P(:,:,i) * J_T(:,:,i);
+    end
+end
+
+subplot(2,2,1);
+colormap('jet');
+scatter(points_keyframe_camera(1,valid_points), points_keyframe_camera(2,valid_points), 2, errors);
+colorbar();
+title('errors in original (keyframe) view');
+
+subplot(2,2,2);
+scatter(points_keyframe_camera(1,valid_points), points_keyframe_camera(2,valid_points), 2, intensities_keyframe);
+colorbar();
+title('keyframe intensity image');
+
+subplot(2,2,3);
+scatter(points_current_camera(1,:), points_current_camera(2,:), 2, errors);
+colorbar();
+title('errors of warped points (in current view)');
+
+subplot(2,2,4);
+scatter(points_current_camera(1,:), points_current_camera(2,:), 2, intensities_keyframe);
+colorbar();
+title('warped keyframe intensity image');
+
+end
+
--- matlab/test_scripts/test_simple_warping.m
+++ matlab/test_scripts/test_simple_warping.m
@@ -16,41 +16,11 @@ FOCAL = 0.035 * size(I1,2) / 0.032;
 
 intrinsics = CameraIntrinsics(size(I1,2), size(I1,1), FOCAL);
 
-intensities1 = image_to_list(I1);
-depths1      = image_to_list(D1);
-
-[X,Y] = meshgrid(1:size(I1,2), 1:size(I1,1));
-
-points_keyframe_camera = image_to_list(cat(3,X,Y));
-
-points_world          = camera_project_inverse(points_keyframe_camera, depths1, intrinsics);
-points_current        = camera_transform(points_world, T);
-points_current_camera = camera_project(points_current, intrinsics);
-
-% filter points outside of image range
-valid_points          = is_in_img_range(points_current_camera, I2);
-points_current_camera = points_current_camera(:, valid_points);
-intensities2          = camera_intensity_sample(points_current_camera, I2);
-
-% plot keyframe points in current frame
-%scatter(points_current_camera(:,1), points_current_camera(:,2), 2, intensities2);
-%colormap('gray');
-%hold on; plot([1 size(I1,2) size(I1,2) 1 1], [1 1 size(I1,1) size(I1,1) 1], '-'); hold off;
-%set(gca, 'Ydir', 'reverse'); % 0,0 is at the top left
-
-% only compare points that projected inside the current image
-intensities1 = intensities1(:, valid_points);
-err = (intensities1 - intensities2).^2;
+% do the actual calculations
+err = camera_warp(I1,D1,I2,T,intrinsics);
 
 %figure;
-subplot(2,1,1);
-colormap('jet');
-scatter(points_keyframe_camera(1,valid_points), points_keyframe_camera(2,valid_points), 2, err);
-colorbar();
-title('errors in original (keyframe) view');
-subplot(2,1,2);
-scatter(points_current_camera(1,:), points_current_camera(2,:), 2, err);
-colorbar();
-title('errors of warped points (in current view)');
+
+disp(['total error: ' num2str(sqrt(sum(err)))]);
 
 %points_cc_valid = points_current_camera(is_in_img_range(points_current_camera, I2), :);
